237bae1074e39acf2ea33e467e4cdf38def989f6
Justin Vasselli <JVasselli@users.noreply.github.com>
2022-09-05 16:07:08 +0900

    Adding tests for read_lines

--- tests/test_util.py
+++ tests/test_util.py
@@ -60,3 +60,98 @@ class TestBufferLines:
                 assert len(result_lines) == buffer_size
                 
         assert num_repetitions == expected_repetitions
+        
+
+class TestReadLines:
+    def test_type_error1(self):
+        with pytest.raises(TypeError):
+            for result_lines in utils.read_lines(input=12, buffer_size=1):
+                assert result_lines == None
+    
+    def test_type_error2(self, tmp_path):
+        path = tmp_path / "test.txt"
+        path.write_text("-")
+        
+        with pytest.raises(TypeError):
+            for result_lines in utils.read_lines(input=path, buffer_size='1'):
+                assert result_lines == ['-']
+                
+    def test_type_error3(self, tmp_path):
+        path = tmp_path / "test.txt"
+        path.write_text("-")
+        
+        with pytest.raises(TypeError):
+            for result_lines in utils.read_lines(input=path, buffer_size=1, progress=100):
+                assert result_lines == ['-']
+                
+    def test_no_file(self, tmp_path):
+        path = tmp_path / "test.txt"
+        
+        with pytest.raises(FileNotFoundError):
+            for result_lines in utils.read_lines(input=path, buffer_size=1):
+                assert result_lines == None
+                
+    def test_empty_file(self, tmp_path):
+        path = tmp_path / "test.txt"
+        path.write_text("")
+        
+        with pytest.raises(StopIteration):
+            result_lines = utils.read_lines(input=path, buffer_size=1)
+            next(result_lines)
+            
+    def test_can_stop(self, tmp_path):
+        lines = ["-"] * 4
+        content = "\n".join(lines)
+        path = tmp_path / "test.txt"
+        path.write_text(content)
+        assert path.read_text() == content
+        
+        result_lines = utils.read_lines(input=path, buffer_size=1)
+        assert len(next(result_lines)) == 1
+        assert len(next(result_lines)) == 1
+        assert len(next(result_lines)) == 1
+        assert len(next(result_lines)) == 1
+        with pytest.raises(StopIteration):
+            next(result_lines)
+            
+    def test1(self, tmp_path, capsys):
+        self.generic_test(4, 4, True, tmp_path, capsys)
+        
+    def test2(self, tmp_path, capsys):
+        self.generic_test(16, 4, False, tmp_path, capsys)
+        
+    def test3(self, tmp_path, capsys):
+        self.generic_test(1, 4, True, tmp_path, capsys)
+    
+    def test4(self, tmp_path, capsys):
+        self.generic_test(17, 4, True, tmp_path, capsys)
+        
+    def test5(self, tmp_path, capsys):
+        self.generic_test(4, 1, True, tmp_path, capsys)
+        
+    
+    def generic_test(self, num_lines, buffer_size, progress, tmp_path, capsys):
+        expected_repetitions = math.ceil(num_lines / buffer_size)
+        
+        lines = ["-"] * num_lines
+        content = "\n".join(lines)
+        path = tmp_path / "test.txt"
+        path.write_text(content)
+        assert path.read_text() == content
+
+        num_repetitions = 0
+        for result_lines in utils.read_lines(input=path, buffer_size=buffer_size, progress=progress):
+            num_repetitions += 1
+            if num_repetitions == expected_repetitions:
+                assert len(result_lines) <= buffer_size
+            else:
+                assert len(result_lines) == buffer_size
+                
+        assert num_repetitions == expected_repetitions
+        
+        if progress:
+            captured_progress = capsys.readouterr().err
+            progress_split = captured_progress.split('\r')
+            assert progress_split[-1].startswith('{}it'.format(num_lines))
+        else:
+            assert capsys.readouterr().err == ''
