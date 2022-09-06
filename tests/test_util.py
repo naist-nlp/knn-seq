d1a359bef74926fa0504a96410c940207c34a568
Justin Vasselli <JVasselli@users.noreply.github.com>
2022-09-06 11:45:34 +0900

    Adding tests for to_device

--- tests/test_util.py
+++ tests/test_util.py
@@ -42,73 +42,49 @@ class TestBufferLines:
 
 class TestReadLines:
     def test_type_error1(self):
+    @pytest.fixture
+    def filepath(self, tmp_path) -> str:
+        path = tmp_path / "test.txt"
+        return path
+    
+    def test_type_error_input(self):
         with pytest.raises(TypeError):
             for result_lines in utils.read_lines(input=12, buffer_size=1):
                 assert result_lines == None
     
-    def test_type_error2(self, tmp_path):
-        path = tmp_path / "test.txt"
-        path.write_text("-")
+    @pytest.mark.parametrize('buffer_size, progress', [('4', True), (1, 100)])                
+    def test_type_errors(self, filepath, buffer_size, progress):
+        filepath.write_text('-')
         
         with pytest.raises(TypeError):
-            for result_lines in utils.read_lines(input=path, buffer_size='1'):
+            for result_lines in utils.read_lines(input=filepath, buffer_size=buffer_size, progress=progress):
                 assert result_lines == ['-']
-                
-    def test_type_error3(self, tmp_path):
-        path = tmp_path / "test.txt"
-        path.write_text("-")
-        
-        with pytest.raises(TypeError):
-            for result_lines in utils.read_lines(input=path, buffer_size=1, progress=100):
-                assert result_lines == ['-']
-                
-    def test_no_file(self, tmp_path):
-        path = tmp_path / "test.txt"
-        
+                                
+    def test_no_file(self, filepath):
         with pytest.raises(FileNotFoundError):
-            for result_lines in utils.read_lines(input=path, buffer_size=1):
+            for result_lines in utils.read_lines(input=filepath, buffer_size=1):
                 assert result_lines == None
                 
-    def test_empty_file(self, tmp_path):
-        path = tmp_path / "test.txt"
-        path.write_text("")
+    def test_empty_file(self, filepath):
+        filepath.write_text("")
         
         with pytest.raises(StopIteration):
-            result_lines = utils.read_lines(input=path, buffer_size=1)
+            result_lines = utils.read_lines(input=filepath, buffer_size=1)
             next(result_lines)
             
-    def test_can_stop(self, tmp_path):
-        lines = ["-"] * 4
+    def test_can_stop(self, filepath):
+        lines = ["-"] * 3
         content = "\n".join(lines)
-        path = tmp_path / "test.txt"
-        path.write_text(content)
-        assert path.read_text() == content
+        filepath.write_text(content)
         
-        result_lines = utils.read_lines(input=path, buffer_size=1)
-        assert len(next(result_lines)) == 1
-        assert len(next(result_lines)) == 1
-        assert len(next(result_lines)) == 1
+        result_lines = utils.read_lines(input=filepath, buffer_size=2)
+        assert len(next(result_lines)) == 2
         assert len(next(result_lines)) == 1
         with pytest.raises(StopIteration):
             next(result_lines)
-            
-    def test1(self, tmp_path, capsys):
-        self.generic_test(4, 4, True, tmp_path, capsys)
         
-    def test2(self, tmp_path, capsys):
-        self.generic_test(16, 4, False, tmp_path, capsys)
-        
-    def test3(self, tmp_path, capsys):
-        self.generic_test(1, 4, True, tmp_path, capsys)
-    
-    def test4(self, tmp_path, capsys):
-        self.generic_test(17, 4, True, tmp_path, capsys)
-        
-    def test5(self, tmp_path, capsys):
-        self.generic_test(4, 1, True, tmp_path, capsys)
-        
-    
-    def generic_test(self, num_lines, buffer_size, progress, tmp_path, capsys):
+    @pytest.mark.parametrize('num_lines,buffer_size,progress', [(1, 4, True), (4, 4, True), (16, 4, True), (17, 4, True), (4, 1, True), (1, 4, False), (4, 4, False), (16, 4, False), (17, 4, False), (4, 1, False)])
+    def test(self, num_lines, buffer_size, progress, tmp_path, capsys):
         expected_repetitions = math.ceil(num_lines / buffer_size)
         
         lines = ["-"] * num_lines
@@ -146,6 +122,15 @@ def scale(x: float, y: float) -> float:
     return x * y
             
 class TestParallelApply:
+    #Fails but shouldn't
+    def test_no_workers(self):
+        def double(x: float) -> float:
+            return x * 2
+            
+        with pytest.raises(ValueError):
+            result = utils.parallel_apply(double, [0.0, 1.0, 2.0, 3.0], 0)
+            next(result)
+            
     def test_simple(self):
         result = utils.parallel_apply(double_it, [0.0, 1.0, 2.0, 3.0])
         assert next(result) == 0
@@ -202,59 +187,40 @@ class TestParallelApply:
         with pytest.raises(TypeError):
             result = utils.parallel_apply(simple, [0.0, 1.0, 2.0, 3.0], 1)
             next(result)
-        
-    def test_with_read_lines(self, tmp_path):
+            
+    @pytest.fixture
+    def tmp_file(self, tmp_path):
         lines = ["a"] * 5
         content = "\n".join(lines)
         path = tmp_path / "test.txt"
         path.write_text(content)
-        assert path.read_text() == content
+        return path
         
+    def test_with_read_lines(self, tmp_file):
         result = utils.parallel_apply( 
             caps,
-            utils.read_lines(path, 3),
+            utils.read_lines(tmp_file, 3),
             1)
         assert next(result) == ["A\n", "A\n", "A\n"]
         assert next(result) == ["A\n", "A"]
         with pytest.raises(StopIteration):
             next(result)
             
-    #Fails but shouldn't
-    def test_no_workers(self):
-        def double(x: float) -> float:
-            return x * 2
-            
-        with pytest.raises(ValueError):
-            result = utils.parallel_apply(double, [0.0, 1.0, 2.0, 3.0], 0)
-            next(result)
-            
           
-    def test_with_two_workers(self, tmp_path):
-        lines = ["a"] * 5
-        content = "\n".join(lines)
-        path = tmp_path / "test.txt"
-        path.write_text(content)
-        assert path.read_text() == content
-        
+    def test_with_two_workers(self, tmp_file):
         result = utils.parallel_apply( 
                 caps,
-                utils.read_lines(path, 3),
+                utils.read_lines(tmp_file, 3),
                 2)
                 
         assert next(result) == ["A\n", "A\n", "A\n", "A\n", "A"]
         with pytest.raises(StopIteration):
             next(result)
             
-    def test_with_two_workers2(self, tmp_path):
-        lines = ["a"] * 5
-        content = "\n".join(lines)
-        path = tmp_path / "test.txt"
-        path.write_text(content)
-        assert path.read_text() == content
-        
+    def test_with_two_workers2(self, tmp_file):
         result = utils.parallel_apply( 
                 caps,
-                utils.read_lines(path, 1),
+                utils.read_lines(tmp_file, 1),
                 2)
                 
         assert next(result) == ["A\n", "A\n"]
@@ -263,22 +229,16 @@ class TestParallelApply:
         with pytest.raises(StopIteration):
             next(result)
             
-    def test_with_too_many_workers(self, tmp_path):
-        lines = ["a"] * 5
-        content = "\n".join(lines)
-        path = tmp_path / "test.txt"
-        path.write_text(content)
-        assert path.read_text() == content
-        
+    def test_with_too_many_workers(self, tmp_file):
         result = utils.parallel_apply( 
                 caps,
-                utils.read_lines(path, 3),
+                utils.read_lines(tmp_file, 3),
                 12)
                 
         assert next(result) == ["A\n", "A\n", "A\n", "A\n", "A"]
         with pytest.raises(StopIteration):
             next(result)
-            
+
 class TestToNDArray:
     def test_ndarray(self):
         array = np.arange(5)
@@ -317,6 +277,56 @@ class TestToNDArray:
     def test_wrong_type(self):
         array = "0, 1, 2, 3, 4"
         with pytest.raises(TypeError):
-            result = utils.to_ndarray(array)    
+            result = utils.to_ndarray(array)      
+      
+class TestToDevice:
+    #Note:  The documentation of this function says 'arbitrary data structures', but not every kind of data structure is available... Maybe we need to make the documentation more explicit?
+    
+    @pytest.fixture
+    def tmp_tensor(self):
+        return torch.arange(5)
         
+    @pytest.mark.parametrize('from_gpu,to_gpu', [(True, True), (True, False), (False, True), (False, False)])
+    def test_tensor(self, tmp_tensor, from_gpu, to_gpu):
+        if not torch.cuda.is_available() and (to_gpu or from_gpu):
+            warnings.warn("No CUDA available, this test always passes")
+            return
             
+        if from_gpu:
+            tmp_tensor.cuda()
+        else:
+            tmp_tensor.cpu()
+            
+        result = utils.to_device(tmp_tensor, use_gpu=to_gpu)
+        
+        if to_gpu:
+            assert result.device == torch.device(type='cuda')
+        else:
+            assert result.device == torch.device(type='cpu')
+            
+    @pytest.fixture
+    def tmp_tensor_dict(self):
+        temp_dict = {}
+        for i in range(5):
+            temp_dict[i] = torch.rand((3, 2)).cpu()
+            
+        return temp_dict
+        
+    @pytest.mark.parametrize('from_gpu,to_gpu', [(True, True), (True, False), (False, True), (False, False)])
+    def test_tensor_dict(self, tmp_tensor_dict, from_gpu, to_gpu):
+        if not torch.cuda.is_available() and (to_gpu or from_gpu):
+            warnings.warn("No CUDA available, this test always passes")
+            return
+            
+        if from_gpu:
+            tmp_tensor_dict = {k : v.cuda() for k, v in tmp_tensor_dict.items()}
+            
+        result = utils.to_device(tmp_tensor_dict, use_gpu=to_gpu)
+        
+        if to_gpu:
+            for k, v in tmp_tensor_dict:
+                assert v.device == torch.device(type='cuda')
+        else:
+            for k, v in tmp_tensor_dict:
+                assert v.device == torch.device(type='cpu')
+                
