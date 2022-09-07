6946be62e6808abb8f67dfec5d4b1876bfeb28d4
Justin Vasselli <JVasselli@users.noreply.github.com>
2022-09-07 13:44:38 +0900

    black reformatting

--- knn_seq/test_utils.py
+++ knn_seq/test_utils.py
@@ -8,28 +8,30 @@ import time
 
 
 class TestBufferLines:
-    @pytest.mark.parametrize('lines,buffer_size', [(4, 4), ([4], '4')])
+    @pytest.mark.parametrize("lines,buffer_size", [(4, 4), ([4], "4")])
     def test_type_errors(self, lines, buffer_size):
         with pytest.raises(TypeError):
             for result_lines in utils.buffer_lines(lines=lines, buffer_size=buffer_size):
                 assert result_lines == None
-                                
+
     def test_zero_lines(self):
         with pytest.raises(StopIteration):
             result_lines = utils.buffer_lines(lines=[])
             next(result_lines)
-            
-    @pytest.mark.parametrize('buffer_size', [-1, 0])
+
+    @pytest.mark.parametrize("buffer_size", [-1, 0])
     def test_zero_buffer(self, buffer_size):
         with pytest.raises(ValueError):
             lines = [1] * 20
             result_lines = utils.buffer_lines(lines=lines, buffer_size=buffer_size)
             next(result_lines)
-            
-    @pytest.mark.parametrize('num_lines,buffer_size', [(1, 4), (4, 4), (16, 4), (17, 4), (4, 1)])
+
+    @pytest.mark.parametrize(
+        "num_lines,buffer_size", [(1, 4), (4, 4), (16, 4), (17, 4), (4, 1)]
+    )
     def test(self, num_lines, buffer_size):
         expected_repetitions = math.ceil(num_lines / buffer_size)
-        
+
         lines = [1] * num_lines
         num_repetitions = 0
         for result_lines in utils.buffer_lines(lines=lines, buffer_size=buffer_size):
@@ -38,5 +40,6 @@ class TestBufferLines:
                 assert len(result_lines) <= buffer_size
             else:
                 assert len(result_lines) == buffer_size
-                
+
         assert num_repetitions == expected_repetitions
+        
