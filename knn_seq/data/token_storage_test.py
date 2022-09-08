52e91838c344dd45e4fe15b512c5e11ef1eba79e
TomokiHoshino <hoshino.tomoki.ho6@is.naist.jp>
2022-09-08 20:21:22 +0900

    black isort

--- knn_seq/data/token_storage_test.py
+++ knn_seq/data/token_storage_test.py
@@ -1,17 +1,10 @@
+import numpy as np
 import pytest
 
-import numpy as np
+from knn_seq.data.token_storage import TokenStorage, make_offsets
 
-
-from knn_seq.data.token_storage import (
-    make_offsets, TokenStorage
-)
 
 def test_make_offsets():
-    arr1=np.array(
-        [1,1,1,1,1,5,4,3,2,1]
-    )
-    arr2=np.array(
-        [0,1,2,3,4,5,10,14,17,19,20]
-    )
-    assert np.array_equal(make_offsets(arr1),arr2)
+    arr1 = np.array([1, 1, 1, 1, 1, 5, 4, 3, 2, 1])
+    arr2 = np.array([0, 1, 2, 3, 4, 5, 10, 14, 17, 19, 20])
+    assert np.array_equal(make_offsets(arr1), arr2)
