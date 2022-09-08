ca6691bb2070bf6c23f6a4e0de1e8fd1fe75af18
TomokiHoshino <hoshino.tomoki.ho6@is.naist.jp>
2022-09-08 20:32:25 +0900

    fix token_storage_test

--- knn_seq/data/token_storage_test.py
+++ knn_seq/data/token_storage_test.py
@@ -7,13 +7,6 @@ import pytest
 
 from knn_seq.data.token_storage import TokenStorage, make_offsets
 
-
-def test_make_offsets():
-    arr1 = np.array([1, 1, 1, 1, 1, 5, 4, 3, 2, 1])
-    arr2 = np.array([0, 1, 2, 3, 4, 5, 10, 14, 17, 19, 20])
-    assert np.array_equal(make_offsets(arr1), arr2)
-
-
 #########################################
 ##### Test class TokenStorage
 #########################################
