053942e249c1f206fd648967a461c57f770f9f37
ping <hay.miz9712@gmail.com>
2022-09-08 15:03:07 +0900

    Delete comments residue.

--- knn_seq/models/fairseq_knn_model_base_test.py
+++ knn_seq/models/fairseq_knn_model_base_test.py
@@ -1,28 +1,7 @@
-import pytest
-
 from . import fairseq_knn_model_base
 
 
 class TestFairseqKNNModelBase:
-    # @pytest.fixture
-    # def init_models(self):
-    #     """Load pre-trained model for test."""
-
-    #     scriptdir = os.path.dirname(os.path.abspath(__file__))
-    #     os.chdir(scriptdir)
-
-    #     filenames = ["../../data/checkpoints/checkpoint_best.pt"]
-
-    #     parser = options.get_generation_parser()
-    #     args = options.parse_args_and_arch(parser, input_args=["../../data/data-bin"])
-    #     cfg = convert_namespace_to_omegaconf(args)
-    #     task = tasks.setup_task(cfg.task)
-
-    #     models, saved_args = load_model_ensemble(filenames, task=task)
-    #     self.knn_base = fairseq_knn_model_base.FairseqKNNModelBase(models)
-
-    #     return saved_args
-
     def test_fit_embed_dim(self, init_models) -> None:
         """test for get_embed_dim()."""
 
