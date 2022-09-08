ec3217f89246856c791dc0d30cee2d0279dc58a0
ping <hay.miz9712@gmail.com>
2022-09-08 14:47:33 +0900

    Divide model load function into conftest.py.

---
+++ knn_seq/models/conftest.py
@@ -0,0 +1,25 @@
+import os
+
+import pytest
+from fairseq import options, tasks
+from fairseq.checkpoint_utils import load_model_ensemble
+from fairseq.dataclass.utils import convert_namespace_to_omegaconf
+
+
+@pytest.fixture
+def init_models():
+    """Load pre-trained model for test."""
+
+    scriptdir = os.path.dirname(os.path.abspath(__file__))
+    os.chdir(scriptdir)
+
+    filenames = ["../../data/checkpoints/checkpoint_best.pt"]
+
+    parser = options.get_generation_parser()
+    args = options.parse_args_and_arch(parser, input_args=["../../data/data-bin"])
+    cfg = convert_namespace_to_omegaconf(args)
+    task = tasks.setup_task(cfg.task)
+
+    models, saved_args = load_model_ensemble(filenames, task=task)
+
+    return models, saved_args
