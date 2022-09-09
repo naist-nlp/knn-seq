106e52c125ad4c27ce5ce4f33dbb13f4ca8984cf
TomokiHoshino <hoshino.tomoki.ho6@is.naist.jp>
2022-09-09 11:38:50 +0900

    　add test_make_offsets

--- knn_seq/data/token_storage_test.py
+++ knn_seq/data/token_storage_test.py
@@ -7,6 +7,13 @@ import pytest
 
 from knn_seq.data.token_storage import TokenStorage, make_offsets
 
+
+def test_make_offsets():
+    arr1 = np.array([1, 1, 1, 1, 1, 5, 4, 3, 2, 1])
+    arr2 = np.array([0, 1, 2, 3, 4, 5, 10, 14, 17, 19, 20])
+    assert np.array_equal(make_offsets(arr1), arr2)
+
+
 POPULATION = list(range(100))
 
 
