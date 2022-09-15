15ef16243ffbeabd5c20188f6080c1b92aa35b6b
Justin Vasselli <JVasselli@users.noreply.github.com>
2022-09-15 17:21:25 +0900

    Changes fixture scopes to make them more available
    Also changed the names of tmp_ to testdata_.  And fixed a test case with a larger source/target pad_to_length

--- knn_seq/test_dataset_wrapper.py
+++ knn_seq/test_dataset_wrapper.py
@@ -8,40 +8,52 @@ import numpy as np
 import torch
 
 
-DATA_DIR = os.path.join(
-    os.path.dirname(os.path.dirname(os.path.realpath(__file__))),
-    "data",
-)
+DATABIN_DIR = os.path.join('data', "data-bin")
 
-DATABIN_DIR = os.path.join(DATA_DIR, "data-bin")
+@pytest.fixture(scope="module")
+def testdata_src_dict():
+    dict_path = os.path.join(DATABIN_DIR, "dict.en.txt")
+    return Dictionary.load(dict_path)
 
+
+@pytest.fixture(scope="module")
+def testdata_tgt_dict():
+    dict_path = os.path.join(DATABIN_DIR, "dict.ja.txt")
+    return Dictionary.load(dict_path)
+
+@pytest.fixture(scope="module")
+def testdata_samples(testdata_src_dict, testdata_tgt_dict):
+    with open(os.path.join('data', "train.en"), "r") as open_in:
+        src_sents = [
+            testdata_src_dict.encode_line(s.strip()) for s in open_in.readlines()
+        ]
+
+    with open(os.path.join('data', "train.ja"), "r") as open_in:
+        tgt_sents = [
+            testdata_tgt_dict.encode_line(s.strip()) for s in open_in.readlines()
+        ]
+
+    samples = []
+    for i in range(10):
+        samples.append({"id": i, "source": src_sents[i], "target": tgt_sents[i]})
+    return samples
 
 class TestLanguagePairDatasetWithRawSentence:
     @pytest.fixture
-    def tmp_src_dict(self):
-        dict_path = os.path.join(DATABIN_DIR, "dict.en.txt")
-        return Dictionary.load(dict_path)
-
-    @pytest.fixture
-    def tmp_tgt_dict(self):
-        dict_path = os.path.join(DATABIN_DIR, "dict.ja.txt")
-        return Dictionary.load(dict_path)
-
-    @pytest.fixture
-    def tmp_dataset(self, tmp_src_dict, tmp_tgt_dict):
-        with open(os.path.join(DATA_DIR, "train.en"), "r") as open_in:
+    def testdata_dataset(self, testdata_src_dict, testdata_tgt_dict):
+        with open(os.path.join('data', "train.en"), "r") as open_in:
             src_sents = [s.strip() for s in open_in.readlines()]
 
-        with open(os.path.join(DATA_DIR, "train.ja"), "r") as open_in:
+        with open(os.path.join('data', "train.ja"), "r") as open_in:
             tgt_sents = [s.strip() for s in open_in.readlines()]
 
         dataset = load_langpair_dataset(
             DATABIN_DIR,
             "train",
             "en",
-            tmp_src_dict,
+            testdata_src_dict,
             "ja",
-            tmp_tgt_dict,
+            testdata_tgt_dict,
             combine=True,
             dataset_impl="mmap",
             upsample_primary=-1,
@@ -58,67 +70,44 @@ class TestLanguagePairDatasetWithRawSentence:
         language_pair_dataset = LanguagePairDatasetWithRawSentence(
             dataset, src_sents=src_sents, tgt_sents=tgt_sents
         )
+        return language_pair_dataset
 
-        assert language_pair_dataset.src_dict == tmp_src_dict
-        assert language_pair_dataset.tgt_dict == tmp_tgt_dict
+    def test_dataset_init(self, testdata_dataset, testdata_src_dict, testdata_tgt_dict):
+        assert testdata_dataset.src_dict == testdata_src_dict
+        assert testdata_dataset.tgt_dict == testdata_tgt_dict
         assert np.array_equal(
-            language_pair_dataset.src_sizes[:10],
+            testdata_dataset.src_sizes[:10],
             np.array([13, 10, 8, 17, 20, 16, 20, 15, 17, 9]),
         )
         assert np.array_equal(
-            language_pair_dataset.tgt_sizes[:10],
+            testdata_dataset.tgt_sizes[:10],
             np.array([9, 6, 8, 7, 9, 9, 8, 5, 6, 8]),
         )
-        assert language_pair_dataset.src_sents == src_sents
-        assert language_pair_dataset.tgt_sents == tgt_sents
-        return language_pair_dataset
+        
+        with open(os.path.join('data', "train.en"), "r") as open_in:
+            src_sents = [s.strip() for s in open_in.readlines()]
 
-    @pytest.fixture
-    def tmp_samples(self, tmp_dataset, tmp_src_dict, tmp_tgt_dict):
-        with open(os.path.join(DATA_DIR, "train.en"), "r") as open_in:
-            src_sents = [
-                tmp_src_dict.encode_line(s.strip()) for s in open_in.readlines()
-            ]
-        assert torch.equal(
-            src_sents[0],
-            tmp_src_dict.encode_line("Please respond ."),  # [130, 131, 132, 121,   2]
-        )
-        assert torch.equal(
-            src_sents[9],
-            tmp_src_dict.encode_line("He is kind ."),  # [155,  26, 156, 121,   2]
-        )
+        with open(os.path.join('data', "train.ja"), "r") as open_in:
+            tgt_sents = [s.strip() for s in open_in.readlines()]
 
-        with open(os.path.join(DATA_DIR, "train.ja"), "r") as open_in:
-            tgt_sents = [
-                tmp_tgt_dict.encode_line(s.strip()) for s in open_in.readlines()
-            ]
-        assert torch.equal(
-            tgt_sents[0],
-            tmp_tgt_dict.encode_line("お 返事 ください 。"),  # [170, 928, 929, 930,   2]
-        )
-        assert torch.equal(
-            tgt_sents[9],
-            tmp_tgt_dict.encode_line("優しい 人 です 。"),  # [947, 332, 948, 930,   2]
-        )
-
-        samples = []
-        for i in range(10):
-            samples.append({"id": i, "source": src_sents[i], "target": tgt_sents[i]})
-        assert len(samples) == 10
-        return samples
+        assert testdata_dataset.src_sents == src_sents
+        assert testdata_dataset.tgt_sents == tgt_sents
 
     @pytest.mark.parametrize(
         ("pad_to_length"),
-        [None, {"source": 1, "target": 1}, {"source": 10, "target": 5}],
-    )  # , {'source' : 10, 'target': 10}])
-    def test_collater(self, tmp_dataset, tmp_samples, pad_to_length):
-
-        collator = tmp_dataset.collater(tmp_samples, pad_to_length=None)
+        [
+            None, 
+            {"source": 1, "target": 1}, 
+            {"source": 10, "target": 6}
+        ],
+    ) 
+    def test_collater(self, testdata_dataset, testdata_samples, pad_to_length):
+        collator = testdata_dataset.collater(testdata_samples, pad_to_length=pad_to_length)
         assert torch.equal(collator["id"], torch.tensor([4, 6, 8, 5, 7, 1, 2, 3, 9, 0]))
         assert collator["ntokens"] == sum([3, 4, 4, 4, 8, 5, 6, 5, 6, 4])
-        assert torch.equal(
-            collator["net_input"]["src_tokens"],
-            torch.tensor(
+        
+        if not pad_to_length or pad_to_length['source'] < 10:
+            expected_src = torch.tensor(
                 [
                     [138, 26, 139, 140, 141, 140, 142, 121, 2],
                     [138, 145, 33, 146, 147, 121, 2, 1, 1],
@@ -132,15 +121,31 @@ class TestLanguagePairDatasetWithRawSentence:
                     [128, 129, 121, 2, 1, 1, 1, 1, 1],
                 ],
                 dtype=torch.int32,
-            ),
-        )
+            )
+        elif pad_to_length['source'] == 10:
+            expected_src = torch.tensor(
+                [
+                    [138, 26, 139, 140, 141, 140, 142, 121, 2, 1],
+                    [138, 145, 33, 146, 147, 121, 2, 1, 1, 1],
+                    [151, 152, 33, 153, 154, 121, 2, 1, 1, 1],
+                    [128, 143, 131, 144, 121, 2, 1, 1, 1, 1],
+                    [148, 149, 150, 144, 121, 2, 1, 1, 1, 1],
+                    [130, 131, 132, 121, 2, 1, 1, 1, 1, 1],
+                    [133, 10, 134, 135, 2, 1, 1, 1, 1, 1],
+                    [136, 26, 137, 121, 2, 1, 1, 1, 1, 1],
+                    [155, 26, 156, 121, 2, 1, 1, 1, 1, 1],
+                    [128, 129, 121, 2, 1, 1, 1, 1, 1, 1],
+                ],
+                dtype=torch.int32,
+            )
+        assert torch.equal(collator["net_input"]["src_tokens"], expected_src)
         assert torch.equal(
             collator["net_input"]["src_lengths"],
             torch.tensor([9, 7, 7, 6, 6, 5, 5, 5, 5, 4]),
         )
-        assert torch.equal(
-            collator["target"],
-            torch.tensor(
+
+        if not pad_to_length or pad_to_length['target'] < 5:
+            expected_tgt = torch.tensor(
                 [
                     [937, 938, 939, 930, 2],
                     [941, 942, 943, 930, 2],
@@ -154,11 +159,8 @@ class TestLanguagePairDatasetWithRawSentence:
                     [170, 928, 929, 930, 2],
                 ],
                 dtype=torch.int32,
-            ),
-        )
-        assert torch.equal(
-            collator["net_input"]["prev_output_tokens"],
-            torch.tensor(
+            )
+            shifted_tgt = torch.tensor(
                 [
                     [2, 937, 938, 939, 930],
                     [2, 941, 942, 943, 930],
@@ -172,8 +174,41 @@ class TestLanguagePairDatasetWithRawSentence:
                     [2, 170, 928, 929, 930],
                 ],
                 dtype=torch.int32,
-            ),  # Right shifted by 1 tgt tokens
-        )
+            )
+        elif pad_to_length['target'] == 6:
+            expected_tgt = torch.tensor(
+                [
+                    [937, 938, 939, 930, 2, 1],
+                    [941, 942, 943, 930, 2, 1],
+                    [332, 51, 946, 930, 2, 1],
+                    [940, 47, 929, 930, 2, 1],
+                    [944, 945, 930, 2, 1, 1],
+                    [931, 932, 315, 930, 2, 1],
+                    [933, 934, 26, 930, 2, 1],
+                    [935, 173, 936, 930, 2, 1],
+                    [947, 332, 948, 930, 2, 1],
+                    [170, 928, 929, 930, 2, 1],
+                ],
+                dtype=torch.int32,
+            )
+            shifted_tgt = torch.tensor(
+                [
+                    [2, 937, 938, 939, 930, 1],
+                    [2, 941, 942, 943, 930, 1],
+                    [2, 332, 51, 946, 930, 1],
+                    [2, 940, 47, 929, 930, 1],
+                    [2, 944, 945, 930, 1, 1],
+                    [2, 931, 932, 315, 930, 1],
+                    [2, 933, 934, 26, 930, 1],
+                    [2, 935, 173, 936, 930, 1],
+                    [2, 947, 332, 948, 930, 1],
+                    [2, 170, 928, 929, 930, 1],
+                ],
+                dtype=torch.int32,
+            )
+        assert torch.equal(collator["target"], expected_tgt)
+        assert torch.equal(collator["net_input"]["prev_output_tokens"], shifted_tgt)
+
         assert collator["src_sents"] == [
             "It is still as cold as ever .",
             "It rains in some places .",
@@ -198,4 +233,3 @@ class TestLanguagePairDatasetWithRawSentence:
             "優しい 人 です 。",
             "お 返事 ください 。",
         ]
-        
