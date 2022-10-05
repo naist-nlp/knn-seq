048f5fea04a65b057d32ce4adc028c4cc4c76270
ping <hay.miz9712@gmail.com>
2022-10-05 14:50:09 +0900

    Add test_extract_sentence_features

--- knn_seq/models/fairseq_knn_model_base_test.py
+++ knn_seq/models/fairseq_knn_model_base_test.py
@@ -3,23 +3,22 @@ from typing import List
 import pytest
 import torch
 
-from . import fairseq_knn_model_base
-
 from data.fixtures import testdata_models
+from knn_seq.models.fairseq_knn_model_base import FairseqKNNModelBase
+
 
 class TestFairseqKNNModelBase:
     @pytest.fixture
     def init_knn_model_base(self, testdata_models) -> None:
         self.ensemble, self.saved_args = testdata_models
-        self.knn_base = fairseq_knn_model_base.FairseqKNNModelBase(self.ensemble)
+        self.knn_base = FairseqKNNModelBase(self.ensemble)
 
     def test_get_embed_dim(self, init_knn_model_base) -> None:
-
         embed_dims = self.knn_base.get_embed_dim()
-        test_case: List[int] = [
+        expected_embed_dims = [
             model.decoder.embed_tokens.embedding_dim for model in self.ensemble
         ]
-        assert embed_dims == test_case
+        assert embed_dims == expected_embed_dims
 
     def test_extract_sentence_features_from_encoder_outs(
         self, init_knn_model_base
@@ -34,7 +33,7 @@ class TestFairseqKNNModelBase:
 
         # Encoder out shape test
         src_tokens = torch.LongTensor([[4, 5, 6]])
-        src_lengths = torch.Tensor([1])
+        src_lengths = torch.Tensor([3])
 
         encoder_outs = [
             model.encoder(src_tokens=src_tokens, src_lengths=src_lengths)
@@ -45,8 +44,25 @@ class TestFairseqKNNModelBase:
         )
         feature_sizes = [list(feature.size()) for feature in sentence_features]
 
-        test_case: List[List[int]] = [
+        expected_feature_sizes: List[List[int]] = [
             [src_tokens.size()[0], model.encoder.embed_tokens.embedding_dim]
             for model in self.ensemble
         ]
-        assert feature_sizes == test_case
+        assert feature_sizes == expected_feature_sizes
+
+    def test_extract_sentence_features(self, init_knn_model_base):
+        net_inputs = {
+            "src_tokens": torch.LongTensor([[4, 5, 6]]),
+            "src_lengths": torch.Tensor([3]),
+        }
+        encoder_features = self.knn_base.extract_sentence_features(net_inputs)
+        feature_sizes = [feature.size() for feature in encoder_features]
+
+        src_num, *_ = net_inputs["src_tokens"].size()
+        embed_dims = [
+            model.encoder.embed_tokens.embedding_dim for model in self.ensemble
+        ]
+        expected_feature_sizes = [
+            torch.zeros(src_num, embed_dim).size() for embed_dim in embed_dims
+        ]
+        assert feature_sizes == expected_feature_sizes
