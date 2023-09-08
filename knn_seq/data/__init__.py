6acf8210f736f5a6c85b37e75063bbd6159c0ac1
Hiroyuki Deguchi <deguchi.hiroyuki.db0@is.naist.jp>
2023-09-08 14:43:22 +0900

    Rename class/module/script
    Changed the style of naming rules about the datastore and its implementations

--- knn_seq/data/__init__.py
+++ knn_seq/data/__init__.py
@@ -1,4 +1,4 @@
-from knn_seq.data.datastore import Datastore
+from knn_seq.data.key_storage import KeyStorage
 from knn_seq.data.token_storage import TokenStorage
 
-__all__ = ["Datastore", "TokenStorage"]
+__all__ = ["KeyStorage", "TokenStorage"]
