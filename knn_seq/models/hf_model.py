3047c726651d9675815b5aa1df34df5ac8c1bd3a
Hiroyuki Deguchi <deguchi.hiroyuki.db0@is.naist.jp>
2023-09-12 12:06:06 +0900

    Bug fix for the condition of the device name

--- knn_seq/models/hf_model.py
+++ knn_seq/models/hf_model.py
@@ -30,7 +30,7 @@ class HFModelBase(nn.Module):
         self.eval()
 
     def cuda(self, device=None):
-        self.tokenizer.device = f"cuda:{device}" or "cpu"
+        self.tokenizer.device = f"cuda:{device}" if device is not None else "cuda"
         return super().cuda(device=device)
 
     def forward(self, *args, **kwargs) -> Tensor:
